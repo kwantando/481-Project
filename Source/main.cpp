c3cdeb4a859aefd2f2052b259139f2ebbf799523
Oleksandr Tyberkevych <otyberke@umich.edu>
2015-02-23 17:50:28 -0500

    commit to main

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,9 +1,17 @@
 #include "Controller.h"
 
+//#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+
 int main()
 {
 
-	Controller main_ctrl{ "filedata.txt" };
+	sf::Music mus;
+	std::cout << mus.openFromFile("Victors.wav") << std::endl;
+
+	Controller main_ctrl{ "Victors.wav",
+						  "C:\\Users\\oleks\\Dropbox\\eecs481\\Project\\MainProjectRepo\\Source\\victors.txt" };
 	main_ctrl.start_reading_input();
 
     return EXIT_SUCCESS;
