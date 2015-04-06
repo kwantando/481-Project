bdaa42395540149a1bcb3dbde42f256f8df64d13
dshafes <dshafes@umich.edu>
2015-04-06 18:05:25 -0400

    added #includes

--- Source/Light_cont.cpp
+++ Source/Light_cont.cpp
@@ -1,4 +1,14 @@
 #include "Light_cont.h"
+#include <stdio.h>    /* Standard input/output definitions */
+#include <stdlib.h> 
+#include <stdint.h>   /* Standard types */
+#include <string.h>   /* String function definitions */
+#include <unistd.h>   /* UNIX standard function definitions */
+#include <fcntl.h>    /* File control definitions */
+#include <errno.h>    /* Error number definitions */
+#include <termios.h>  /* POSIX terminal control definitions */
+#include <sys/ioctl.h>
+#include <getopt.h>
 #include "Arduino_cont.h"
 #include <string>
 
