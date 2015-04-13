0a5aae1d7e6f22c2ca61275022ad73f93dee8d16
beamed <beamed@umich.edu>
2015-04-13 18:15:40 -0400

    fix name of window

--- Source/Game_board.cpp
+++ Source/Game_board.cpp
@@ -9,7 +9,7 @@ Game_board::Game_board(sf::RenderWindow*& event_ptr) //:
 //light_controller("/dev/tty.usbmodem1421")
 {
 	event_ptr = event_window = new sf::RenderWindow(sf::VideoMode(930, 625),
-			                            "Event_Handler_Window",
+			                            "MelodiMemori",
 			                            sf::Style::Fullscreen);
 
 	if (!button_texture.loadFromFile("button300.png",
