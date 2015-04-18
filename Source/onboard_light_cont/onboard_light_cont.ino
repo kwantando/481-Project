7621c49aa53dd75cc447330bc983fc531285f042
dshafes <dshafes@umich.edu>
2015-04-18 19:31:55 -0400

    fixed version of arduino code

--- Source/onboard_light_cont/onboard_light_cont.ino
+++ Source/onboard_light_cont/onboard_light_cont.ino
@@ -2,7 +2,7 @@ void setup() {
   // initialize serial communication:
   Serial.begin(9600); 
    // initialize the LED pins:
-      for (int thisPin = 2; thisPin < 9; thisPin++) {
+      for (int thisPin = 2; thisPin <=8; thisPin++) {
         pinMode(thisPin, OUTPUT);
       } 
 }
@@ -18,45 +18,45 @@ void loop() {
     // example 'a' = 97, 'b' = 98, and so forth:
 
     switch (inByte) {
-    case 'a'ar:    
-      digitalWrite(2, LOW);
+    case 'a':    
+      digitalWrite(2, HIGH);
       break;
     case 'b':    
-      digitalWrite(4, HIGH);
-      break;
-    case 'c':    
-      digitalWrite(6, HIGH);
-      break;
-    case 'd':    
       digitalWrite(3, HIGH);
       break;
-    case 'e':    
+    case 'c':    
+      digitalWrite(4, HIGH);
+      break;
+    case 'd':    
       digitalWrite(5, HIGH);
       break;
+    case 'e':    
+      digitalWrite(6, HIGH);
+      break;
     case 'f':
-      digitalWrite(9, HIGH);
+      digitalWrite(7, HIGH);
       break;
     case 'A':
       digitalWrite(2, LOW);
       break;
     case 'B':
-      digitalWrite(4, LOW);
-      break;
-    case 'C':
-      digitalWrite(6, LOW);
-      break;
-    case 'D':
       digitalWrite(3, LOW);
       break;
-    case 'E':
+    case 'C':
+      digitalWrite(4, LOW);
+      break;
+    case 'D':
       digitalWrite(5, LOW);
       break;
+    case 'E':
+      digitalWrite(6, LOW);
+      break;
     case 'F':
-      digitalWrite(9, LOW);
+      digitalWrite(7, LOW);
       break;
     default:
       // turn all the LEDs off:
-      for (int thisPin = 2; thisPin < 7; thisPin++) {
+      for (int thisPin = 2; thisPin <= 9; thisPin++) {
         digitalWrite(thisPin, LOW);
       }
     } 
